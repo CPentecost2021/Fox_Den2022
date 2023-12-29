3b4bf4e9a837bf19dc02b3b667dd81ff0c9aa0a5
CPentecost2021 <70184050+CPentecost2021@users.noreply.github.com>
2023-12-28 21:00:00 -0500

    updated js script for new launch Date

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,24 @@
 // Countdown timer function
 function countdown() {
-    const launchDate = new Date('2024-01-01T00:00:00Z').getTime(); // Set your launch date here
-    const now = new Date().getTime();
-    const distance = launchDate - now;
-  
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-  
-    if (distance < 0) {
-      clearInterval(timer);
-      document.getElementById('countdown').innerHTML = 'Site is live!';
-    }
+  const launchDate = new Date('2024-01-07T00:00:00Z').getTime(); // Updated launch date to January 7, 2024
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById('countdown').innerHTML = 'Site is live!';
   }
-  
-  // Update the countdown every second
-  const timer = setInterval(countdown, 1000);
-  
-  // Initial call to start countdown immediately
-  countdown();
+}
+
+// Update the countdown every second
+const timer = setInterval(countdown, 1000);
+
+// Initial call to start countdown immediately
+countdown();
